c3e0a58aa0b4c920dc9413f53396b86c45e5734a
JaylaH202 <153013890+JaylaH202@users.noreply.github.com>
2026-05-20 14:21:21 -0400

    Update routes.py

--- routes.py
+++ routes.py
@@ -274,7 +274,7 @@ def profile():
 
     # Pull the account for display
     # Note the two closing parentheses: ))
-	cursor.execute('SELECT user_id FROM accounts WHERE username = %s', (username,))
+    cursor.execute('SELECT user_id FROM accounts WHERE username = %s', (username,))
 	result = cursor.fetchone()
 
 
